3e7364f2c4f605fc60429e6088cea0cc0b726807
Alexis Priore <alexispriore@gmail.com>
2020-01-23 19:47:56 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Paitent
+  
+  attr_accessor :name, :appointment, :doctor
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.paitent == self}
+  end
+  
+  def doctors 
+    appointments.collect {|appointment| appointment.doctor}
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
+  
+end
